a0388a299ea7ec1ee0e3e0435e4cc881128ad294
valmsmith39a <valmsmith39a@gmail.com>
2016-04-24 22:58:42 -0700

    creates Posts reducer to rootReducer

--- public/lib/reducers/index.js
+++ public/lib/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import PostsReducer from './reducer_posts';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  posts: PostsReducer
+});
 
 export default rootReducer;
