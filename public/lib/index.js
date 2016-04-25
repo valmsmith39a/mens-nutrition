2c2afd2302d9a4857848de1ebd6758fe644d2d44
valmsmith39a <valmsmith39a@gmail.com>
2016-04-24 22:03:39 -0700

    adds README.md, installs and sets up React Router (with routes file) and Redux (with root reducer)

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,1 +1,19 @@
-console.log('hello from index.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers/index';
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = compose(
+  applyMiddleware(promise)
+)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
+)
