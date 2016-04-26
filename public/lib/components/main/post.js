8b52aa6993aa24b1930d2b26bcc79be08ffcaf7b
valmsmith39a <valmsmith39a@gmail.com>
2016-04-25 19:40:51 -0700

    refactors post.js from class level component to functional component, destructures arguments to access url directly, use ES6 to further shorten code

--- public/lib/components/main/post.js
+++ public/lib/components/main/post.js
@@ -1,14 +1,3 @@
 import React from 'react';
 
-export default class Post extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('this.props.post', this.props);
-  }
-
-  render() {
-    return (
-      <li>{this.props.post.url}</li>
-    );
-  }
-}
+export default ({ post: { url }, index }) => <li>{url}</li>
