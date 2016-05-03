04dae6e935cf0bf611eec3290375861b3f9ff09a
valmsmith39a <valmsmith39a@gmail.com>
2016-05-02 23:10:57 -0700

    modifies reducer to remove the post by the index in DELETE_POST case of reducer_posts.js

--- public/lib/reducers/reducer_posts.js
+++ public/lib/reducers/reducer_posts.js
@@ -9,12 +9,13 @@ const INITIAL_STATE = [];
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case GET_ALL_POSTS:
+      // action.payload.data is array of posts
       return action.payload.data;
     case CREATE_POST:
+      // action.payload.data is the post created
       return [...state, action.payload.data];
     case DELETE_POST:
-      // TESTING: temprarily set index to delete to 1
-      const index = 1;
+      const index = action.payload.data;
       return [ ...state.slice(0, index), ...state.slice(1,index + 1) ];
     default:
       return state;
