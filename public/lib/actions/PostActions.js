9687987d29c0a45302e74b616f49b8e33e87e121
valmsmith39a <valmsmith39a@gmail.com>
2016-05-05 00:15:36 -0700

    cleans up action creator code

--- public/lib/actions/PostActions.js
+++ public/lib/actions/PostActions.js
@@ -26,18 +26,9 @@ export function createPost(post) {
   }
 }
 
-// export function deletePost(id, index) {
-//   const request = axios.delete(`${DELETE_POST_URL}/${id}/${index}`);
-//
-//   return {
-//     type: DELETE_POST,
-//     payload: request
-//   }
-// }
-
 export const deletePost = (id, index) => {
   const request = axios.delete(`${DELETE_POST_URL}/${id}/${index}`);
-  
+
   return {
     type: DELETE_POST,
     payload: request
