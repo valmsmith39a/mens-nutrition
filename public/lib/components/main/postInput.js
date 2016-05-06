de21bd7b470f5b693b7a64a60722b09cb7008fbe
valmsmith39a <valmsmith39a@gmail.com>
2016-05-05 19:20:24 -0700

    Cleans up code and clears input box for refactoring postInput from class level to functional component

--- public/lib/components/main/postInput.js
+++ public/lib/components/main/postInput.js
@@ -15,7 +15,7 @@ let PostInput = ({ dispatch }) => {
           return
         }
         dispatch(createPost({url: input.value}))
-        // input.value = ''
+        input.value = ''
       }}>
         <input ref={node => {
           input = node
@@ -31,38 +31,3 @@ let PostInput = ({ dispatch }) => {
 PostInput = connect()(PostInput)
 
 export default PostInput
-
-
-
-
-
-// <div id='input-box-wrapper'>
-//   <input id='input-box' type='text' placeholder='Web URL. Example: www.cnn.com' />
-//   <button type='button' className='btn btn-primary' id='input-btn' onClick={ e => {
-//       e.preventDefault();
-//       dispatch(createPost({url: 'some url'}))
-//     }
-//   }>
-//   Post
-//   </button>
-// </div>
-// class PostInput extends React.Component {
-//   createPost() {
-//     const postObject = {
-//       url: this.refs.inputData.value
-//     }
-//     this.props.createPost(postObject)
-//       .then(response => {});
-//   }
-//
-//   render() {
-//     return (
-//       <div id='input-box-wrapper'>
-//         <input id='input-box' type='text' placeholder='Web URL. Example: www.cnn.com' ref='inputData' />
-//         <button type='button' className='btn btn-primary' id='input-btn' onClick={this.createPost.bind(this)}>Post</button>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default connect (null, { createPost })(PostInput);
