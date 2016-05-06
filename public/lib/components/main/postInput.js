07c2783e82a5c260142b3bd9b69d175250651b23
valmsmith39a <valmsmith39a@gmail.com>
2016-05-05 18:57:27 -0700

    sets up refactoring postInput component from class level to functional component

--- public/lib/components/main/postInput.js
+++ public/lib/components/main/postInput.js
@@ -4,23 +4,38 @@ import {
   createPost
 } from '../../actions/PostActions';
 
-class PostInput extends React.Component {
-  createPost() {
-    const postObject = {
-      url: this.refs.inputData.value
-    }
-    this.props.createPost(postObject)
-      .then(response => {});
-  }
-
-  render() {
-    return (
-      <div id='input-box-wrapper'>
-        <input id='input-box' type='text' placeholder='Web URL. Example: www.cnn.com' ref='inputData' />
-        <button type='button' className='btn btn-primary' id='input-btn' onClick={this.createPost.bind(this)}>Post</button>
-      </div>
-    );
-  }
+let PostInput = ({ dispatch }) => {
+  return (
+    <div id='input-box-wrapper'>
+      <input id='input-box' type='text' placeholder='Web URL. Example: www.cnn.com' />
+      <button type='button' className='btn btn-primary' id='input-btn'>
+      Post
+      </button>
+    </div>
+  )
 }
 
-export default connect (null, { createPost })(PostInput);
+PostInput = connect()(PostInput);
+
+export default PostInput
+
+// class PostInput extends React.Component {
+//   createPost() {
+//     const postObject = {
+//       url: this.refs.inputData.value
+//     }
+//     this.props.createPost(postObject)
+//       .then(response => {});
+//   }
+//
+//   render() {
+//     return (
+//       <div id='input-box-wrapper'>
+//         <input id='input-box' type='text' placeholder='Web URL. Example: www.cnn.com' ref='inputData' />
+//         <button type='button' className='btn btn-primary' id='input-btn' onClick={this.createPost.bind(this)}>Post</button>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default connect (null, { createPost })(PostInput);
