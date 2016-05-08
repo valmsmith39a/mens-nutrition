d603eb480ea6c99ae1f12a7a82c15c0e3ec1a092
valmsmith39a <valmsmith39a@gmail.com>
2016-05-07 20:47:31 -0700

    experimenting with refactoring to displayPosts to functional component

--- public/lib/components/main/displayPosts.js
+++ public/lib/components/main/displayPosts.js
@@ -3,47 +3,45 @@ import { connect } from 'react-redux';
 import { getAllPosts } from '../../actions/PostActions';
 import Post from './post';
 
-let DisplayPosts = ( { dispatch }) => {
-  // this.props.getAllPosts()
-  // .then(response => {});
-  dispatch(getAllPosts());
+class DisplayPosts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props.getAllPosts()
+      .then(response => {});
+  }
 
-  return(
-    <div>Display Posts</div>
-  );
+  renderPosts() {
+    return this.props.posts.map((post, index) =>
+        <Post key={index} post={post} index={index} />
+    );
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.renderPosts()}
+      </ul>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
+function mapStateToProps(state) {
   return state;
 }
 
-DisplayPosts = connect(mapStateToProps)(DisplayPosts);
+export default connect(mapStateToProps, { getAllPosts })(DisplayPosts);
 
-export default DisplayPosts;
-// class DisplayPosts extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.props.getAllPosts()
-//       .then(response => {});
-//   }
-//
-//   renderPosts() {
-//     return this.props.posts.map((post, index) =>
-//         <Post key={index} post={post} index={index} />
-//     );
-//   }
-//
-//   render() {
-//     return (
-//       <ul>
-//         {this.renderPosts()}
-//       </ul>
-//     );
-//   }
+// Refactoring from class level to functional component
+// let DisplayPosts = (props) => {
+//   return(
+//     <div>Display Posts</div>
+//   );
 // }
 //
-// function mapStateToProps(state) {
+// const mapStateToProps = (state) => {
 //   return state;
 // }
 //
-// export default connect(mapStateToProps, { getAllPosts })(DisplayPosts);
+// DisplayPosts = connect(mapStateToProps, { getAllPosts })(DisplayPosts);
+//
+// export default DisplayPosts;
