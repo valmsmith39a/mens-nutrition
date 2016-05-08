59745632d2f28a7c7089ec93b5e8d959abd8251b
valmsmith39a <valmsmith39a@gmail.com>
2016-05-07 20:08:05 -0700

    starts refactoring displayPosts.js to functional componenet

--- public/lib/components/main/displayPosts.js
+++ public/lib/components/main/displayPosts.js
@@ -3,30 +3,47 @@ import { connect } from 'react-redux';
 import { getAllPosts } from '../../actions/PostActions';
 import Post from './post';
 
-class DisplayPosts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props.getAllPosts()
-      .then(response => {});
-  }
+let DisplayPosts = ( { dispatch }) => {
+  // this.props.getAllPosts()
+  // .then(response => {});
+  dispatch(getAllPosts());
 
-  renderPosts() {
-    return this.props.posts.map((post, index) =>
-        <Post key={index} post={post} index={index} />
-    );
-  }
-
-  render() {
-    return (
-      <ul>
-        {this.renderPosts()}
-      </ul>
-    );
-  }
+  return(
+    <div>Display Posts</div>
+  );
 }
 
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
   return state;
 }
 
-export default connect(mapStateToProps, { getAllPosts })(DisplayPosts);
+DisplayPosts = connect(mapStateToProps)(DisplayPosts);
+
+export default DisplayPosts;
+// class DisplayPosts extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.props.getAllPosts()
+//       .then(response => {});
+//   }
+//
+//   renderPosts() {
+//     return this.props.posts.map((post, index) =>
+//         <Post key={index} post={post} index={index} />
+//     );
+//   }
+//
+//   render() {
+//     return (
+//       <ul>
+//         {this.renderPosts()}
+//       </ul>
+//     );
+//   }
+// }
+//
+// function mapStateToProps(state) {
+//   return state;
+// }
+//
+// export default connect(mapStateToProps, { getAllPosts })(DisplayPosts);
