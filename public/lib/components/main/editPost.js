262e298f54a39c963336803ce0810458038fb921
valmsmith39a <valmsmith39a@gmail.com>
2016-05-11 17:24:25 -0700

    implements simple show/close edit form

--- public/lib/components/main/editPost.js
+++ public/lib/components/main/editPost.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-let EditPost = function({ dispatch }) {
-  return(
-    <button>Edit</button>
-  );
+class EditPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {show: false};
+  }
+
+  showForm() {
+    this.setState({show: true});
+  }
+
+  closeForm() {
+    this.setState({show: false});
+  }
+
+  showEditForm() {
+    if(this.state.show) {
+      return(
+        <div>
+          <input />
+          <button onClick={this.closeForm.bind(this)}>Save Edit</button>
+        </div>
+      );
+    } else {
+      return (<div></div>);
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.showForm.bind(this)}>Edit
+        </button>
+        {this.showEditForm()}
+      </div>
+    );
+  }
 }
 
-EditPost = connect()(EditPost);
+export default connect()(EditPost);
 
-export default EditPost;
+// let EditPost = function({ dispatch }) {
+//   return(
+//     <button>Edit</button>
+//   );
+// }
+//
+// EditPost = connect()(EditPost);
+//
+// export default EditPost;
