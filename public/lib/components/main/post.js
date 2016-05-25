bb2bb635a86d3c9ed876d96c5a23427dba93c113
valmsmith39a <valmsmith39a@gmail.com>
2016-05-25 00:10:55 -0700

    adjusts edit and delete button positions

--- public/lib/components/main/post.js
+++ public/lib/components/main/post.js
@@ -2,27 +2,19 @@ import React from 'react';
 import DeletePost from './deletePost';
 import EditPost from './editPost';
 
-/*
-<div className='col-md-6 pull-right' id='delete-btn'>
-  <DeletePost index={index} id={post._id} />
-</div>
-<div className='col-md-6' id='edit-btn'>
-  <EditPost index={index} id={post._id} />
-</div>
-*/
 export default ({ post, post: { url }, index }) => (
   <li className='post'>
     <div className='row'>
       {url}
     </div>
     <div className='row'>
-      <div className='btn-group pull-right'>
-      <div className='col-md-6' id='delete-btn'>
-        <DeletePost index={index} id={post._id} />
-      </div>
-      <div className='col-md-6' id='edit-btn'>
-        <EditPost index={index} id={post._id} />
-      </div>
+      <div className='delete-edit-wrapper pull-right'>
+        <div className='col-md-6' id='delete-wrapper'>
+          <DeletePost index={index} id={post._id} />
+        </div>
+        <div className='col-md-6' id='edit-wrapper'>
+          <EditPost index={index} id={post._id} />
+        </div>
       </div>
     </div>
   </li>
