96385caed5ebb149877445d59ab90650446b9cf7
valmsmith39a <valmsmith39a@gmail.com>
2016-05-31 20:31:16 -0700

    adjusts url and delete/edit buttons

--- public/lib/components/main/post.js
+++ public/lib/components/main/post.js
@@ -7,7 +7,7 @@ export default ({ post, post: { url }, index }) => (
     <div className='post-image'>
       <img src='http://i2.wp.com/www.theimpulsivebuy.com/wordpress/wp-content/uploads/2015/03/McDonalds-Artisan-Grilled-Chicken-Sandwich.jpg?resize=640%2C532' className='img-url' />
     </div>
-    <div className='row'>
+    <div className='row post-title'>
       {url}
     </div>
     <div className='row'>
